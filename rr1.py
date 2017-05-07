3b77a5e02514d50590ee8969be048a82994aa0e0
umnaAlvi <bsef15m528@pucit.edu.pk>
2017-05-07 09:36:19 +0000

    Update rr1.py

--- rr1.py
+++ rr1.py
@@ -1,9 +1,6 @@
 #!usr/bin/python
 proc={}
-burst=[]
-At=[]
 total=0
-A_sort=0
 num=input("Enter the number of process:  ")
 Qt=input("Enter Quantum time of processes: ")
 for i in range(0,num):
@@ -13,9 +10,7 @@ for i in range(0,num):
        min=arr	
    elif(min>arr):
        min=arr	
-   At.append(arr)
-   burst.append(bT)
-   proc[i+1]=[At[i],burst[i]]  
+   proc[i+1]=[arr,bT,i+1]  
 total=min
 if(total>0):
 	print  "idle time","0----------",total
@@ -34,7 +29,10 @@ for index in range(1,num+1):
    temp=proc.get(index)[0]
    proc.get(index)[0]=proc.get(exe)[0]
    proc.get(exe)[0]=temp
-   print proc.get(index)
+	
+   temp=proc.get(index)[2]
+   proc.get(index)[2]=proc.get(exe)[2]
+   proc.get(exe)[2]=temp
 rem=0
 count=0
 i=1
@@ -42,46 +40,38 @@ j=num+1
 wait=[]
 turn=[]
 tim=min
-k=1
-rand=[]
-ra=0
 while(count!=num):
-     
       rem=(proc.get(i)[1])-Qt
       if(proc.get(i)[1]>Qt):
-         proc[j]=[proc.get(i)[0],rem]
+         proc[j]=[proc.get(i)[0],rem,i]
          j=j+1
       if(rem>0):
          total=total+Qt
-         print tim,"-----",total
+         print "process ",i," ",tim,"-----",total," "
       if(rem==0):
          total=total+proc.get(i)[1]
-         print tim,"-----",total," "
-         rand.insert(k,total)
-         k=k+1
+         print "process ",i," ",tim,"-----",total," "
+         cntn=proc.get(i)[2]
+         proc1[count+1]=[proc.get(cntn)[0],proc.get(cntn)[1],total]
          count=count+1
-      
       if(rem<0):
          total=total+proc.get(i)[1] 
-         print tim,"-----",total," "
-         
-         rand.insert(k,total)
-         k=k+1
-         
+         print "process ",i," ",tim,"-----",total," "
+         cntn=proc.get(i)[2]
+         proc1[count+1]=[proc.get(cntn)[0],proc.get(cntn)[1],total]
          count=count+1
-      k=k+1
       tim=total
       i=i+1
 A_t=0
 A_w=0 
 for i in range (1,num+1):
-   wait.insert(i-1,(rand[i-1]-proc.get(i)[0]-proc.get(i)[1]))
-   turn.insert(i-1,(rand[i-1]-proc.get(i)[0]))
+   wait.insert(i-1,(proc1.get(i)[2]-proc1.get(i)[0]-proc1.get(i)[1]))
+   turn.insert(i-1,(proc1.get(i)[2]-proc1.get(i)[0]))
    A_t=A_t+turn[i-1]
    A_w=A_w+wait[i-1]
 print "process"," ","Arrival time"," ","burst time"," ","waiting time"," ","turnaround time"
 for i in range(1,num+1):
-   print i,"\t\t",proc.get(i)[0],"\t\t",proc.get(i)[1],"\t\t",wait[i-1],"\t\t",turn[i-1]
+   print proc.get(i)[2],"\t\t",proc1.get(i)[0],"\t\t",proc1.get(i)[1],"\t\t",wait[i-1],"\t\t",turn[i-1]
 print "average waiting time: ",A_w/num," average turnaround time: ",A_t/num 
  
 
