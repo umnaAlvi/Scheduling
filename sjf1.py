2e471c2f5dd7ec090eb1ed80354afbe7b98761e9
umnaAlvi <bsef15m528@pucit.edu.pk>
2017-05-13 01:17:36 +0500

    Update sjf1.py

--- sjf1.py
+++ sjf1.py
@@ -4,10 +4,9 @@ proc1={}
 A_T=0
 B_T=0
 total=0
-k=2
 min1=0
-n=input("enter number of process: ")
-for i in range(0,n):
+num=input("enter number of process: ")
+for i in range(0,num):
    A_T=input("Enter Arrival time: ")
    if(i==0):
       min1=A_T
@@ -16,10 +15,10 @@ for i in range(0,n):
    B_T=input("Enter Burst time: ")
    proc1[i+1]=[A_T,B_T]
 a_time=proc1.get(1)[0]
-for index in range(1,n+1):
+for index in range(1,num+1):
    #min=a_time
    exe=index
-   for j in range(index+1,n+1):
+   for j in range(index+1,num+1):
       if(proc1.get(j)[0]<proc1.get(exe)[0]):
          exe=j
    temp=proc1.get(index)[1]
@@ -30,49 +29,48 @@ for index in range(1,n+1):
    proc1.get(index)[0]=proc1.get(exe)[0]
    proc1.get(exe)[0]=temp
 b_time=0
-k=2
+next1=2
 #accordting t and w
-for j in range(1,n+1):
+for j in range(1,num+1):
   b_time=proc1.get(j)[1]+b_time
-  min=k
-  for i in range(j+1,n+1):  
-       if(b_time>=proc1.get(i)[0] and proc1.get(i)[1]<proc1.get(k)[1]):
-          temp=proc1.get(k)[0]
+  min=next1
+  for i in range(j+1,num+1):  
+       if(b_time>=proc1.get(i)[0] and proc1.get(i)[1]<proc1.get(next1)[1]):
+          temp=proc1.get(next1)[0]
           proc1.get(k)[0]=proc1.get(i)[0]
           proc1.get(i)[0]=temp
      
-          temp=proc1.get(k)[1]
-          proc1.get(k)[1]=proc1.get(i)[1]
+          temp=proc1.get(next1)[1]
+          proc1.get(next1)[1]=proc1.get(i)[1]
           proc1.get(i)[1]=temp
-  k=k+1
+  next1=next1+1
 print "process in sjf order"
-for i in range(1,n+1):
+for i in range(1,num+1):
   print proc1.get(i)[0]," ",proc1.get(i)[1]
 a_time=proc1.get(1)[0]
 total=min1
 b_time=0
-tol=total
-tim=total
-wit=0
-trt=0
+sum1=total
+min2=total
+wait_time=0
+turn_time=0
 wait=[]
 turn=[]
-j=0
 A_w=0
 A_t=0
 
-for i in range(1,n+1):
-   tim=tol
-   wit=tim
-   wait.insert(i-1,(tim-(proc1.get(i)[0])))
+for i in range(1,num+1):
+   min2=sum1
+   wait_time=min2
+   wait.insert(i-1,(min2-(proc1.get(i)[0])))
    b_time=proc1.get(i)[1]
-   tol=tol+b_time;
-   print tim,"----------------",tol
-   turn.insert(i-1,(tol-proc1.get(i)[0]))
+   sum1=sum1+b_time;
+   print min2,"----------------",sum1
+   turn.insert(i-1,(sum1-proc1.get(i)[0]))
    A_w=A_w+wait[i-1]
    A_t=A_t+turn[i-1]
    
 print "process"," ","Arrival time"," ","burst time"," ","waiting time"," ","turnaround time"
-for i in range(1,n+1):
+for i in range(1,num+1):
    print i,"\t\t",proc1.get(i)[0],"\t\t",proc1.get(i)[1],"\t\t",wait[i-1],"\t\t",turn[i-1]
 print "avg waitng time ",A_w/n,"avg turnaround time:  ",A_t/n
